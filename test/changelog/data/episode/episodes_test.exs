4f4bf1a9bfac777b8a8bd60c9ef473832ec1a436
JoeBew42 <joebew42@gmail.com>
2018-04-01 12:36:17 +0200

    extract a context

--- test/changelog/data/episode/episodes_test.exs
+++ test/changelog/data/episode/episodes_test.exs
@@ -5,46 +5,41 @@ defmodule Changelog.EpisodesTest do
   import ChangelogWeb.TimeView, only: [hours_ago: 1]
 
   alias Changelog.{CalendarService, CalendarEvent}
-
   alias Changelog.Episodes
 
+  setup do
+    episode_params = %{slug: "181", title: "some content", recorded_at: hours_ago(1)}
+    podcast = insert(:podcast)
+    expected_event = %CalendarEvent{
+      name: "Recording '#{podcast.name}'",
+      start: Map.get(episode_params, :recorded_at),
+      duration: 90,
+      location: "Skype",
+      notes: "Setup guide: https://changelog.com/guest/#{podcast.slug}"
+    }
+
+    %{
+      episode_params: episode_params,
+      podcast: podcast,
+      expected_event: expected_event
+    }
+  end
+
   describe "when create an episode" do
-    test "and a calendar event is successfully created a calendar event id is saved" do
+    test "and a calendar event is successfully created a calendar event id is saved", context do
       with_mock(CalendarService, [create: fn(_) -> {:ok, "EVENT_ID"} end]) do
-        episode_params = %{slug: "181", title: "some content", recorded_at: hours_ago(1)}
-        podcast = insert(:podcast)
+        {:ok, episode} = Episodes.create(context.episode_params, context.podcast)
 
-        expected_event = %CalendarEvent{
-          name: "Recording '#{podcast.name}'",
-          start: Map.get(episode_params, :recorded_at),
-          duration: 90,
-          location: "Skype",
-          notes: "Setup guide: https://changelog.com/guest/#{podcast.slug}"
-        }
-
-        {:ok, episode} = Episodes.create(episode_params, podcast)
-
-        assert called CalendarService.create(expected_event)
+        assert called CalendarService.create(context.expected_event)
         assert episode.calendar_event_id == "EVENT_ID"
       end
     end
 
-    test "and a calendar event is not created a calendar event id is not saved" do
+    test "and a calendar event is not created a calendar event id is not saved", context do
       with_mock(CalendarService, [create: fn(_) -> {:error, "unable to create the event"} end]) do
-        episode_params = %{slug: "181", title: "some content", recorded_at: hours_ago(1)}
-        podcast = insert(:podcast)
+        {:ok, episode} = Episodes.create(context.episode_params, context.podcast)
 
-        expected_event = %CalendarEvent{
-          name: "Recording '#{podcast.name}'",
-          start: Map.get(episode_params, :recorded_at),
-          duration: 90,
-          location: "Skype",
-          notes: "Setup guide: https://changelog.com/guest/#{podcast.slug}"
-        }
-
-        {:ok, episode} = Episodes.create(episode_params, podcast)
-
-        assert called CalendarService.create(expected_event)
+        assert called CalendarService.create(context.expected_event)
         assert episode.calendar_event_id == nil
       end
     end
