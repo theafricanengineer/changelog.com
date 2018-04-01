f32115d771dae4f8a85d2ef49f7713565b95fdc8
JoeBew42 <joebew42@gmail.com>
2018-04-01 21:35:09 +0200

    remove the @google_calendar_timezone

--- test/changelog/services/google_calendar_service_test.exs
+++ test/changelog/services/google_calendar_service_test.exs
@@ -6,10 +6,9 @@ defmodule Changelog.Services.GoogleCalendarServiceTest do
   alias Changelog.Services.GoogleCalendarService
 
   @google_calendar_id "bdo60t3hcbpqsgkehhef8j9sqo@group.calendar.google.com"
-  @google_calendar_timezone "Europe/Rome"
 
   test "#create" do
-    event_start_at = Timex.to_datetime({{2018, 4, 1}, {11, 00, 00}}, @google_calendar_timezone)
+    event_start_at = Timex.to_datetime({{2018, 4, 1}, {11, 00, 00}}, "UTC")
     calendar_event = %CalendarEvent{
       name: "A calendar event name",
       start: event_start_at,
@@ -34,11 +33,16 @@ defmodule Changelog.Services.GoogleCalendarServiceTest do
     assert google_calendar_event.summary == calendar_event.name
     assert google_calendar_event.description == calendar_event.notes
     assert google_calendar_event.location == calendar_event.location
-    assert google_calendar_event.start.dateTime == Timex.format!(calendar_event.start, "{ISO:Extended}")
-    assert google_calendar_event.end.dateTime == Timex.format!(Timex.add(calendar_event.start, Timex.Duration.from_minutes(calendar_event.duration)), "{ISO:Extended}")
+    assert parse_as_utc_datetime(google_calendar_event.start.dateTime) == calendar_event.start
+    assert parse_as_utc_datetime(google_calendar_event.end.dateTime) == Timex.add(calendar_event.start, Timex.Duration.from_minutes(calendar_event.duration))
 
     Enum.map(google_calendar_event.attendees, & &1.email)
     |> Enum.each(& assert Enum.member?(calendar_event.attendees, %{email: &1}))
+  end
+
+  defp parse_as_utc_datetime(iso_date) do
+    Timex.parse!(iso_date, "{ISO:Extended}")
+     |> Timex.Timezone.convert("UTC")
   end
 
   defp google_api_connection do
