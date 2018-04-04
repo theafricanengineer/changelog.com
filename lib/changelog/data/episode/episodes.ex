49b6c745aef08669f5000b06ec7c8be28832109a
JoeBew42 <joebew42@gmail.com>
2018-04-04 19:17:24 +0200

    remove the calendar_service as argument

--- lib/changelog/data/episode/episodes.ex
+++ lib/changelog/data/episode/episodes.ex
@@ -6,24 +6,24 @@ defmodule Changelog.Episodes do
   alias Changelog.Episode
   alias Changelog.CalendarEvent
 
-  def create(episode_params, podcast, calendar_service \\ @calendar_service) do
+  def create(episode_params, podcast) do
     result = build_assoc(podcast, :episodes)
       |> Episode.preload_all
       |> Episode.admin_changeset(episode_params)
       |> Repo.insert
 
     case result do
-      {:ok, episode} -> publish_calendar_event_for(episode, calendar_service)
+      {:ok, episode} -> publish_calendar_event_for(episode)
       _ -> result
     end
   end
 
-  defp publish_calendar_event_for(episode, calendar_service) do
+  defp publish_calendar_event_for(episode) do
     calendar_event = episode
       |> Episode.preload_all
       |> CalendarEvent.build_for
 
-    case calendar_service.create(calendar_event) do
+    case @calendar_service.create(calendar_event) do
       {:ok, event_id} -> Episode.update_calendar_event_id(episode, event_id)
       {:error, _reason} -> {:ok, episode}
     end
