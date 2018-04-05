7b250490166acc1a5d00fe42e5af349edc4e6617
JoeBew42 <joebew42@gmail.com>
2018-04-05 12:02:37 +0200

    return nil as default value for an event_id

--- lib/changelog/data/episode/calendar_service.ex
+++ lib/changelog/data/episode/calendar_service.ex
@@ -1,6 +1,6 @@
 defmodule Changelog.CalendarService do
   alias Changelog.CalendarEvent
 
-  def create(_event = %CalendarEvent{}), do: {:ok, "EVENT_ID"}
+  def create(_event = %CalendarEvent{}), do: {:ok, nil}
   def delete(_event_id), do: {:ok}
 end
